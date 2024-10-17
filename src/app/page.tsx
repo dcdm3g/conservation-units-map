c9405d734612b5335ec2d3aa0bc327ddb56e1776
Davi Melo <davicosta01082008@gmail.com>
2024-10-16 21:37:42 -0300

    feat: add sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Sidebar } from '@/components/sidebar'
 import { UnitsMap } from '@/components/units-map'
 
 export default function Home() {
 	return (
 		<div className="min-h-screen bg-zinc-950 text-zinc-50 flex">
 			<UnitsMap />
+			<Sidebar />
 		</div>
 	)
 }
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,1 +1,23 @@
-export function Sidebar() {}
+export function Sidebar() {
+	return (
+		<aside className="basis-96 p-6 bg-card flex flex-col gap-4">
+			<h1 className="text-3xl font-bold text-foreground">
+				Conheça as Unidades de Conservação do Rio Grande do Sul
+			</h1>
+
+			<p className="text-foreground">
+				As Unidades de Conservação do Rio Grande do Sul desempenham um papel
+				fundamental na proteção da biodiversidade e dos ecossistemas locais. Com
+				uma variedade que inclui parques, reservas e áreas de proteção
+				ambiental, elas são essenciais para a preservação do patrimônio natural
+				do estado. Essas áreas não apenas conservam a flora e fauna, mas também
+				promovem atividades de pesquisa e educação ambiental.
+			</p>
+
+			<p className="text-foreground font-semibold">
+				Clique em um dos marcadores para saber mais sobre a unidade de
+				conservação.
+			</p>
+		</aside>
+	)
+}
