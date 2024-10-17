7005fd42dafea2813ff7bbedabfbd944106119ec
Davi Melo <davicosta01082008@gmail.com>
2024-10-16 21:19:06 -0300

    feat: add markers

--- src/components/units-map.tsx
+++ src/components/units-map.tsx
@@ -3,6 +3,7 @@
 import mapboxgl, { type Map as MapType } from 'mapbox-gl'
 import { useEffect, useRef } from 'react'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { conservationUnits } from '@/data/conservation-units'
 
 export function UnitsMap() {
 	const mapContainerRef = useRef<HTMLDivElement | null>(null)
@@ -20,6 +21,20 @@ export function UnitsMap() {
 				[-57.6485, -33.7512],
 				[-49.6976, -27.0767],
 			],
+		})
+
+		conservationUnits.forEach((unit) => {
+			const el = document.createElement('div')
+			el.className = 'marker'
+
+			new mapboxgl.Marker()
+				.setLngLat(unit.lngLat)
+				.setPopup(new mapboxgl.Popup({ offset: 25 }).setText(unit.name))
+				.addTo(mapRef.current!)
+
+			el.addEventListener('click', () => {
+				alert(`You clicked on ${unit.name}`)
+			})
 		})
 
 		return () => mapRef.current!.remove()
