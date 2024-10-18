e43c7de65762ab0c1b3b3f7c55f0bbe2daacc181
dorvillethiago <dorville.dev@gmail.com>
2024-10-18 19:43:11 -0300

    feat: replace markers with regions

---
+++ src/components/units-map.tsx
@@ -0,0 +1,79 @@
+'use client'
+
+import mapboxgl, { type Map as MapType } from 'mapbox-gl'
+import { type Dispatch, type SetStateAction, useEffect, useRef } from 'react'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import type { Unit } from '@/interfaces/unit'
+import { GEOJSON } from '@/constants/geojson'
+
+interface UnitsMapProps {
+	setUnit: Dispatch<
+		SetStateAction<Unit | null>
+	>
+}
+
+export function UnitsMap({
+	setUnit,
+}: UnitsMapProps) {
+	const mapContainerRef = useRef<HTMLDivElement | null>(null)
+	const mapRef = useRef<MapType>()
+
+	useEffect(() => {
+		mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!
+
+		mapRef.current = new mapboxgl.Map({
+			container: mapContainerRef.current!,
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [-51.9253, -30.0346],
+			zoom: 6,
+			maxBounds: [
+				[-57.6485, -33.7512],
+				[-49.6976, -27.0767],
+			],
+		})
+
+		mapRef.current.on('load', () => {
+			mapRef.current!.addSource('maine', { type: 'geojson', data: GEOJSON })
+
+			mapRef.current!.addLayer({
+				id: 'background',
+				type: 'fill',
+				source: 'maine',
+				paint: {
+					'fill-color': 'hsl(142.1, 76.2%, 36.3%)',
+					'fill-opacity': 0.5,
+				},
+			})
+
+			mapRef.current!.addLayer({
+				id: 'outline',
+				type: 'line',
+				source: 'maine',
+				paint: {
+					'line-color': 'hsl(142.1, 76.2%, 36.3%)',
+					'line-width': 2,
+				},
+			})
+
+			mapRef.current!.on('click', 'background', (el) => {
+				const [feature] = el.features!
+				const unit = feature.properties as Unit
+				setUnit(unit)
+			})
+
+			mapRef.current!.on('mouseenter', 'background', () => {
+				const canvas = mapRef.current!.getCanvas()
+				canvas.style.cursor = 'pointer'
+			})
+
+			mapRef.current!.on('mouseleave', 'background', () => {
+				const canvas = mapRef.current!.getCanvas()
+				canvas.style.cursor = ''
+			})
+		})
+
+		return () => mapRef.current!.remove()
+	}, [setUnit])
+
+	return <div className="flex-1 bg-muted" ref={mapContainerRef} />
+}
