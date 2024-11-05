89159c0966d813ebec9a2949876ced2639771ccb
Davi Melo <davicosta01082008@gmail.com>
2024-11-05 09:27:35 -0300

    style: fix biome errors

--- src/interfaces/unit.ts
+++ src/interfaces/unit.ts
@@ -1,6 +1,6 @@
 export interface Unit {
-  name: string
-  summary: string
-  category: 'Unidade de Uso Sustentável' | 'Unidade de Proteção Integral'
-  lngLat?: [number, number]
+	name: string
+	summary: string
+	category: 'Unidade de Uso Sustentável' | 'Unidade de Proteção Integral'
+	lngLat?: [number, number]
 }
