1ec102c18ac46238e2caba848227849b5ec1a64b
Davi Melo <davicosta01082008@gmail.com>
2024-11-05 20:07:50 -0300

    fix: fix floating units search errors

--- src/components/floating-units-search.tsx
+++ src/components/floating-units-search.tsx
@@ -10,7 +10,7 @@ export function FloatingUnitsSearch() {
 	const [search, setSearch] = useState('')
 
 	return (
-		<div className="absolute inset-4 md:hidden z-40 flex flex-col">
+		<div className="absolute top-4 inset-x-4 md:hidden z-40 flex flex-col">
 			<Input
 				className="rounded-b-none focus-visible:ring-0 focus-visible:ring-offset-0"
 				value={search}
@@ -18,22 +18,20 @@ export function FloatingUnitsSearch() {
 				placeholder="Pesquise pelas UCs..."
 			/>
 
-			<ScrollArea className="flex-1">
-				<ul className="space-y-2 rounded-b bg-background">
-					{Object.values(UNITS)
-						.filter(
-							(unit) =>
-								search &&
-								unit.name.toLowerCase().includes(search.toLowerCase()),
-						)
-						.slice(0, 5)
-						.map((unit) => (
-							<li key={unit.name}>
-								<UnitCard unit={unit} onClick={() => setSearch('')} />
-							</li>
-						))}
-				</ul>
-			</ScrollArea>
+				<ScrollArea className="flex-1">
+					<ul className="space-y-2 rounded-b bg-background">
+						{Object.values(UNITS)
+							.filter((unit) =>
+								search && unit.name.toLowerCase().includes(search.toLowerCase()),
+							)
+							.slice(0, 5)
+							.map((unit) => (
+								<li key={unit.name}>
+									<UnitCard unit={unit} onClick={() => setSearch('')} />
+								</li>
+							))}
+					</ul>
+				</ScrollArea>
 		</div>
 	)
 }
