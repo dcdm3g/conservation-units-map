7c2a30dc015af5a9e747c59f266f0bfceb70b3cc
Thiago Dorville <dorville.dev@gmail.com>
2024-12-04 09:00:16 -0300

    feat: add unit titles

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -3,6 +3,11 @@ import type { MDXComponents } from 'mdx/types'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
 	return {
 		...components,
+		h1: ({ children }) => {
+			return (
+				<h1 className="text-3xl font-bold my-4 text-justify">{children}</h1>
+			)
+		},
 		h2: ({ children }) => {
 			return (
 				<h2 className="text-2xl font-bold my-4 text-justify">{children}</h2>
